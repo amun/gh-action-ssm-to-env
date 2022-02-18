fa788a1bcff2f80bdd8fe8ed1c21aafb1c7f6465
Kalvis Kuskis <44435644+kalvkusk@users.noreply.github.com>
2022-02-18 16:48:11 +0200

    add prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,29 @@
 
 IFS=$'\n'
 SSM_PATH=$1
+PREFIX=$2
 aws ssm get-parameters-by-path --with-decryption \
-    --path $1 \
+    --path $SSM_PATH \
     --query "Parameters[*].{Name:Name,Value:Value}" > params.json
 
 VARNAMES=($(cat params.json  | jq -r ".[].Name" | sed 's~'$SSM_PATH'~~g'))
 VALUES=($(cat params.json  | jq ".[].Value" | sed 's~'$SSM_PATH'~~g'))
 
-for i in "${!VARNAMES[@]}"; 
-do 
-    echo "${VARNAMES[$i]}=${VALUES[$i]}" >> $GITHUB_ENV
-    echo "::add-mask::${VALUES[$i]}"
-done
+if [ -n "$PREFIX" ]; then
+    for i in "${!VARNAMES[@]}"; 
+    do 
+       if [[ "${VARNAMES[$i]}" == *"$PREFIX"* ]]; then
+         echo "${VARNAMES[$i]}=${VALUES[$i]}" >> $GITHUB_ENV
+         echo "::add-mask::${VALUES[$i]}"
+        fi  
+    done
+
+else
+    for i in "${!VARNAMES[@]}"; 
+    do 
+        echo "${VARNAMES[$i]}=${VALUES[$i]}" >> $GITHUB_ENV
+        echo "::add-mask::${VALUES[$i]}"
+    done
+fi
+
+
